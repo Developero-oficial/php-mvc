08b1edcf43871bd6ba84feff2c8e77e3952b17e6
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-04-29 10:21:14 -0500

    Router class

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo $_SERVER['REQUEST_URI'];
+require 'system/core/Router.php';
+
+$router = new Router();
+echo '<pre>';
+print_r($router->getUri());
+echo '</pre>';
+
+$controlador = $router->getController();
+$method = $router->getMethod();
+$param = $router->getParam();
+echo "Controlador: {$controlador} </br>";
+echo "Método: {$method} </br>";
+echo "Param: {$param} </br>";
---
+++ system/core/Router.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Identificacion de la URI
+ */
+class Router
+{
+  /**
+   * @var string
+   */
+  public $uri;
+
+  /**
+   * @var string
+   */
+  public $controller;
+
+  /**
+   * @var string
+   */
+  public $method;
+
+  /**
+   * @var string
+   */
+  public $param;
+
+  /**
+   * Inicializa los atributos
+   */
+  public function __construct()
+  {
+    $this->setUri();
+    $this->setController();
+    $this->setMethod();
+    $this->setParam();
+  }
+
+  /**
+   * Asigna la uri completa
+   */
+  public function setUri()
+  {
+    $this->uri = explode('/', $_SERVER['REQUEST_URI']);
+  }
+
+  /**
+   *Asigna el nombre del controlador
+   */
+  public function setController()
+  {
+    $this->controller = $this->uri[2] === '' ? 'Home' : $this->uri[2];
+  }
+
+  /**
+   * Asigna el nombre del metodo
+   */
+  public function setMethod()
+  {
+    $this->method = ! empty($this->uri[3]) ? $this->uri[3] : 'exec';
+  }
+
+  /**
+   * Asigna el valor del parametro si existe
+   */
+  public function setParam()
+  {
+    $this->param = ! empty($this->uri[4]) ? $this->uri[4] : '';
+  }
+
+  /**
+   * @return $uri
+   */
+  public function getUri()
+  {
+    return $this->uri;
+  }
+
+  /**
+   * @return $controller
+   */
+  public function getController()
+  {
+    return $this->controller;
+  }
+
+  /**
+   * @return $method
+   */
+  public function getMethod()
+  {
+    return $this->method;
+  }
+
+  /**
+   * @return $param
+   */
+  public function getParam()
+  {
+    return $this->param;
+  }
+}

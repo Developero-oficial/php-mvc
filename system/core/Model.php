9ae35da96191f97024021b58633673956ec63ebf
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-07-02 13:16:05 -0500

    Se agrega destructor en model class para desconectarse de la DB

--- system/core/Model.php
+++ system/core/Model.php
@@ -16,4 +16,12 @@ class Model
   {
     $this->db = new Mysqli(HOST, USER, PASSWORD, DB_NAME);
   }
+
+  /**
+  * Finaliza conexion
+  */
+  public function __destruct()
+  {
+    $this->db->close();
+  }
 }

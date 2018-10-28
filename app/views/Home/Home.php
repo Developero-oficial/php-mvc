48e3d78bbd8bbb2ee14e19a5cef76b164ceb0908
liloo2040 <e.dufour.89@gmail.com>
2018-10-28 22:43:12 +0100

    anadido mas diseno a la rama con css

--- app/views/Home/Home.php
+++ app/views/Home/Home.php
@@ -3,15 +3,19 @@
 <html>
 <head>
   <title>Home</title>
+  <meta charset="utf-8" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"> 
+  <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 <body>
   <!-- Main jumbotron for a primary marketing message or call to action -->
-  <div class="jumbotron">
+  <div class="jumbotron header">
     <div class="container text-center">
       <h1>Hola, <?= $nombre ?>!</h1>
       <p>Este es un ejemplo de MVC con Php vanilla hecho desde cero.</p>
-      <p><a class="btn btn-primary btn-lg" target="_blank" href="https://github.com/Developero-oficial/php-mvc" role="button">Código fuente &raquo;</a></p>
+      <p><a class="btn btn-primary btn-lg button" target="_blank" href="https://github.com/Developero-oficial/php-mvc" role="button">Código fuente &raquo;</a></p>
     </div>
   </div>
 </body>
